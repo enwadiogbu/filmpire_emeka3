69dbd25d9542fd19683d36b03fbf54a09fc580fd
Emeka Nwadiogbu, Jr. <emekanwadiogbu.jr@me.com>
2024-01-08 00:58:21 -0700

    second commit using the createRoot for index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
 
 const App = () => {
-  return <div>Hello, World Filmpire</div>;
+  <div>
+    <CssBaseline />
+    <h1>Hello, World - Filmpire</h1>
+  </div>;
 };
 
 export default App;
