91be67e05645aab2043df9078471a25e576d71e0
Emeka Nwadiogbu, Jr. <emekanwadiogbu.jr@me.com>
2024-01-08 03:14:09 -0700

    completed the full component and folder structure and force added the @mui/styles with the --force tag

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { CssBaseline } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 
+import useStyles from "./styles";
 import { Actors, MovieInformation, Movies, Profiles, Navbar } from "./";
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <>
+    <div className={classes.root}>
       <CssBaseline />
       <Navbar />
-      <main>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
         <Routes>
           <Route path="/movie/:id" element={<MovieInformation />} />
         </Routes>
@@ -23,7 +27,7 @@ function App() {
           <Route path="/profiles/:id" element={<Profiles />} />
         </Routes>
       </main>
-    </>
+    </div>
   );
 }
 
