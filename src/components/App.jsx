88109b62dfda517035a3ee98abff7ade059c145a
Emeka Nwadiogbu, Jr. <emekanwadiogbu.jr@me.com>
2024-01-08 02:56:54 -0700

    setup the routing for the main components and navbar with a functional component and reverted back to React 17 for react-dom statements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
-import { Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-const App = () => {
-  <div>
-    <CssBaseline />
-    <h1>Hello, World - Filmpire</h1>
-  </div>;
-};
+import { Actors, MovieInformation, Movies, Profiles, Navbar } from "./";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/movie/:id" element={<MovieInformation />} />
+        </Routes>
+        <Routes>
+          <Route path="/actors/:id" element={<Actors />} />
+        </Routes>
+        <Routes>
+          <Route path="/movies/:id" element={<Movies />} />
+        </Routes>
+        <Routes>
+          <Route path="/profiles/:id" element={<Profiles />} />
+        </Routes>
+      </main>
+    </>
+  );
+}
 
 export default App;
